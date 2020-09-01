cbf874dc6a1b1c66ae07cd20e92467ca5e7ab041
Fuying Wang <thuwfy15@gmail.com>
2020-09-01 16:25:18 +0800

    update hw1

--- hw1/cs285/infrastructure/tf_utils.py
+++ hw1/cs285/infrastructure/tf_utils.py
@@ -26,8 +26,10 @@ def build_mlp(input_placeholder, output_size, scope, n_layers, size, activation=
     output_placeholder = input_placeholder
     with tf.variable_scope(scope):
         for _ in range(n_layers):
-            output_placeholder = TODO # HINT: use tf.layers.dense (specify <input>, <size>, activation=<?>)
-        output_placeholder = TODO # HINT: use tf.layers.dense (specify <input>, <size>, activation=<?>)
+            output_placeholder = tf.layers.dense(inputs=output_placeholder, units=size, activation=activation)
+            # HINT: use tf.layers.dense (specify <input>, <size>, activation=<?>)
+        output_placeholder = tf.layers.dense(inputs=output_placeholder, units=output_size, activation=output_activation) 
+        # HINT: use tf.layers.dense (specify <input>, <size>, activation=<?>)
     return output_placeholder
 
 
